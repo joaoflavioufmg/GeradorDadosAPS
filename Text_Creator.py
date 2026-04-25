6bc333216b41fdb411a1f517dbedf667980cc1de
joaoflavioufmg <joao.flavio@dep.ufmg.br>
2026-04-25 11:44:21 -0300

    SIZE to SIZE0

--- Text_Creator.py
+++ Text_Creator.py
@@ -257,7 +257,7 @@ class text_messages_creator():
         header_text = f"param MAX_HOME_SHC := {self.scenario_data_config.maximo_deslocamento['MAX_HOME_SHC']};\n"
         self.texts_variables.append(header_text)
 
-        header_text = f"param MAX_HOME_THC := {self.scenario_data_config.maximo_deslocamento['MAX_HOME_THC']};\n"
+        header_text = f"param MAX_HOME_THC := {self.scenario_data_config.maximo_deslocamento['MAX_HOME_THC']};\n\n"
         self.texts_variables.append(header_text)
 
     def create_variable_costs_SHC_text(self): 
@@ -776,7 +776,7 @@ class text_messages_creator_By_SC(text_messages_creator):
     def create_variable_costs_PHC_text(self):
         #Dados ainda nao disponiveis, por isso usei o default 80000
         comentary_text = ("# # Variable cost of PHC j / patient\n")
-        header_text = "param:	        ITEM1   SIZE	FC1		VC1:=\n"
+        header_text = "param:	        ITEM1   SIZE0	FC1		VC1:=\n"
         self.texts_variables.append(comentary_text)
         self.texts_variables.append(header_text)
         df_base = self.scenario_dfs[self.scenario_dfs.CO_UNIDADE_UBS > 0].copy()
@@ -1151,7 +1151,7 @@ class text_messages_creator_By_Cluster(text_messages_creator):
     def create_variable_costs_PHC_text(self):
         #Dados ainda nao disponiveis, por isso usei o default 80000
         comentary_text = ("# # Variable cost of PHC j / patient\n")
-        header_text = "param:	        ITEM1   SIZE	FC1		VC1:=\n"
+        header_text = "param:	        ITEM1   SIZE0	FC1		VC1:=\n"
         self.texts_variables.append(comentary_text)
         self.texts_variables.append(header_text)
         df_base = self.df_exist_PHC.copy()
