d95f82c40c6a4b711c17c0f2f825a6698a1750ef
otaviomvasc <96895134+otaviomvasc@users.noreply.github.com>
2026-04-25 10:29:51 -0300

    teste joao

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ PASTAS = {
 }   
 
 if __name__ == "__main__":
+    #TESTE JOAO
     municipio = MUNCIPIO
     dados_setor_censitario_completo = r"dados_censo_FINAL_reduzido.csv"
     tipo_dos_dados = ExecutionDataType.BY_SETOR_CENSITARIO
